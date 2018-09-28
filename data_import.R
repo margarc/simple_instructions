2d84bcdb3616eba2c9cd08dff292a4d31b0f7b19
Margarita <margarita.cariolou@gmail.com>
2018-09-28 15:56:43 +0100

    Update data_import.R

--- data_import.R
+++ data_import.R
@@ -21,11 +21,14 @@ text_to_table <- read.delim("name.txt")
 write.table(text_to_table, 
             file="to_table_example.csv",sep=",",col.names=FALSE,row.names=FALSE)
 
-# We use file="xxxxxxxxxx.csv" to specify the name we want to give to the file that will now have
-# a .csv extension and is going to be saved in our directory
+# We use file="xxxxxxxxxx.csv" to specify the NEW name we want to give to the file which will now have
+# a .csv extension and is going to be saved in our specified directory.
 
-write.table(text_to_table, 
-            file="by_country_example.csv",sep=",",col.names=FALSE,row.names=FALSE)
+text_to_table <- read.delim("by_country.txt")
+text_to_table
+View(text_to_table)
+write.csv(text_to_table,file="margarita_testing.csv", sep=",", col.names=FALSE,row.names=FALSE)
+
 
 
 
